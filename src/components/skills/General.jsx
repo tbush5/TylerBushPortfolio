e59ba345edf552c2060f01917864ab34af860adb
Tyler Bush <tyler.bush.1@slu.edu>
2023-10-02 16:58:45 -0500

    description changes and removal of non used skill section

--- src/components/skills/General.jsx
+++ src/components/skills/General.jsx
@@ -1,73 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const General = () => {
-    return (
-        <div className="skills__content">
-            <h3 className="skills__title">General Skills</h3>
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">General Skills</h3>
 
-            <div className="skills__box">
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
 
-                        <div>
-                            <h3 className="skills__name">Git</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">Unity</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
-                        </div>
-                    </div>
-                    
-                </div>
-
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">Agile</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name">VS Code</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-
-                        <div>
-                            <h3 className="skills__name"></h3>
-                            <span className="skills__level"></span>
-                        </div>
-                    </div>
-                    
-                </div>
+            <div>
+              <h3 className="skills__name">Git</h3>
+              <span className="skills__level">Intermediate</span>
             </div>
-        </div>
-    )
-}
+          </div>
 
-export default General
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Unity</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Agile</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">VS Code</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default General;
